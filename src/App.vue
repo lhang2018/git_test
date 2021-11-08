be07d6d75e3382aae6583b6597253910222d574b
liuhang <liuhang@liuhangdeMacBook-Pro.local>
2021-11-08 17:19:51 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <hello-world v-for="todo in todos" :key="todo.id" :msg="todo.text" :deal="addList"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      msg:'Welcome to Your Vue.js App',
+      todos:[
+        {text:'学习 JS',id:'1'},
+        {text:'学习 Vue',id:'2'},
+        {text:'学习 React',id:'3'},
+        {text:'学习 Angular',id:'4'}
+      ]
+    } 
+  },
+  methods:{
+    addList:function(item){
+      console.log(item)
+      this.todos.push(item)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="dealAddAction" >
+      add
+    </button>
+    <input type="text" v-model="data1" name="" id="">
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['msg','deal'],
+  data(){
+    return {
+      test:'testing',
+      id:'4',
+      data1:''
+    }
+  },
+  methods:{
+    dealAddAction:function(){
+      console.log('1')
+      this.$emit('deal',{text:'add',id:++this.id})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
